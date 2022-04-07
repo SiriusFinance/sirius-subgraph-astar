7cf2db75a8e0df9d6965bd4d285d81c0d7905d76
0xlou <100200877+0xlou@users.noreply.github.com>
2022-04-07 21:04:54 +0800

    fix error

--- src/mappings/swapNormal.ts
+++ src/mappings/swapNormal.ts
@@ -348,7 +348,7 @@ export function handleTokenSwap(event: TokenSwap): void {
         } else {
           return BigDecimal.fromString("0")
         }
-      }).reduce((first, second) => first.plus(second), BigDecimal.fromString("0"))
+      }).reduce((first: BigDecimal, second: BigDecimal) => first.plus(second), BigDecimal.fromString("0"))
       let apy = dailyTotalSwapFees.div(tvl).times(BigDecimal.fromString('365'))
       swap.TVL = tvl
       swap.APY = apy
